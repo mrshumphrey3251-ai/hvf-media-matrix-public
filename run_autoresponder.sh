8d1e71e22afc9c6cb54a0fe4f6fe182fb24da0cf
Jeffery Humphrey <humphreyvirtualfarm@gmail.com>
2026-08-07 13:40:22 -0500

    SECURITY OVERRIDE: Redacted localized environment variables from public blueprint.

--- run_autoresponder.sh
+++ run_autoresponder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # HVF Autonomous Commercial Intake Trigger
 cd /home/mrshumphrey3251/HVF_MEDIA_MATRIX
-export GMAIL_APP_PASSWORD="gdcpowbnsxyarbfj"
+export GMAIL_APP_PASSWORD="[REDACTED_FOR_PUBLIC_AUDIT]"
 echo "--- INTAKE SWEEP INITIATED: $(date) ---" >> autoresponder_cron.log
 python3 commercial_autoresponder.py >> autoresponder_cron.log 2>&1
 echo "--- INTAKE SWEEP CONCLUDED ---" >> autoresponder_cron.log
