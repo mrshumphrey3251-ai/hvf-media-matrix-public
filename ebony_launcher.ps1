5ac7afa200bb2a20a0e883b8f9844d4a9a6ab00d
mrshumphrey3251-ai <mrshumphrey3251@gmail.com>
2026-08-17 10:07:06 -0500

    Deployment 71: Synchronized ignition sequence blueprint

--- ebony_launcher.ps1
+++ ebony_launcher.ps1
@@ -5,5 +5,8 @@ Get-Process | Where-Object {$_.Name -match "python"} | Stop-Process -Force -Erro
 cd C:\HVF_Repos\hvf-media-matrix-private
 Start-Process -FilePath "pythonw.exe" -ArgumentList "hvf_comm_server.py" -WindowStyle Hidden
 
+# Micro-delay: Allow the headless engine time to mount the server
+Start-Sleep -Seconds 3
+
 # Launch the secure browser gate
 Start-Process "http://127.0.0.1:8085"
