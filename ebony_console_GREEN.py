ae81576704a4e486fb8a88f794afc19994f13d9f
mrshumphrey3251-ai <mrshumphrey3251@gmail.com>
2026-08-24 17:04:25 -0500

    release: lock training video into native inline player

--- ebony_console_GREEN.py
+++ ebony_console_GREEN.py
@@ -483,7 +483,7 @@ with tab_farm:
         
         c_vid, c_inst = st.columns([1.5, 1])
         with c_vid:
-            st.video("https://www.youtube.com/watch?v=uK1z7nIqWjQ")
+            st.components.v1.html('<iframe width="100%" height="315" src="https://www.youtube.com/embed/uK1z7nIqWjQ?rel=0&modestbranding=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="border-radius: 8px;"></iframe>', height=325)
             st.caption("*(HVF Master Training: Universal RTMP Stream Setup)*")
         
         with c_inst:
