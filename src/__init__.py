f8f1344173ddf169027773bea1ef9f878f77d160
mrshumphrey3251-ai <mrshumphrey3251@gmail.com>
2026-09-01 14:56:30 -0500

    core: deploy redacted root package initializer blueprint

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+HVF Media Matrix - Root Source Encapsulation (Public Blueprint)
+Master package initialization.
+Internal root routing and engine exposure controls are classified.
+"""
+__version__ = "1.0.0"
+
+# [REDACTED FOR PUBLIC REPOSITORY: Master routing imports hidden]
